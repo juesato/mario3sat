77b31527304c542b500ba93b5d74693543b0dda4
Harrison Okun <haroku32@gmail.com>
2014-10-04 18:03:53 -0400

    Updated todo

--- HackMit/src/Main.java
+++ HackMit/src/Main.java
@@ -35,16 +35,43 @@ public class Main extends Applet implements ActionListener{
 	{
 		if(e.getSource()==input){
 			satInput=input.getText();
-			
+			String[] ors=satInput.split("&");
+			ArrayList<String> vars=new ArrayList<String>();
+			for(String x : ors){
+				Clause clause=new Clause();
+				int start=x.indexOf('(');
+				int stop=x.indexOf('(');
+				x=x.substring(start+1, stop+1);
+				String[] varExps=x.split("|");
+				for(int i=0;i<3;i++){
+					String y=varExps[i];
+					if(y.charAt(0)=='!'){
+						clause.varNegs[i]=false;
+						y=y.substring(1);
+					}
+					if(vars.indexOf(y)>=0){
+						clause.vars[i]=vars.indexOf(y);
+					}
+				}
+			}
 		}
 		
 		
 	}
 	
 	public class Clause{
-		int[] vars=new int[3];
-		boolean[] varNegs=new boolean[3];
-	
+		public int[] vars=new int[3];
+		public boolean[] varNegs={true,true,true};
+		
+		public Clause(){
+			
+		}
+		
+		public Clause(int[] varsIn,boolean[] varNegsIn){
+			this.vars=varsIn;
+			this.varNegs=varNegsIn;
+		}
+		
 	}
 
 }
