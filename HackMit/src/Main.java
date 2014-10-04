e27646f7af826d03e7739a45e2a5f82fe9ea4699
Harrison Okun <haroku32@gmail.com>
2014-10-04 17:41:51 -0400

    Added types for internal representation of sat

--- HackMit/src/Main.java
+++ HackMit/src/Main.java
@@ -4,17 +4,21 @@ import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.*;
+import java.util.ArrayList;
 
 
 public class Main extends Applet implements ActionListener{
 	public String satInput;
+	TextField input=new TextField(40);
+	public int numVars;
+	public ArrayList<Clause> clauses=new ArrayList<Clause>();
 	
 	public void init(){
 		this.setSize(500, 500);
 		this.setLayout(null);
 		Label inputLabel=new Label("Input your 3sat");
 		inputLabel.setBounds(10,10,100,20);
-		TextField input=new TextField(40);
+		
 		input.setBounds(10,40,100,20);
 		this.add(inputLabel);
 		this.add(input);
@@ -29,9 +33,18 @@ public class Main extends Applet implements ActionListener{
 	
 	public void actionPerformed(ActionEvent e)
 	{
+		if(e.getSource()==input){
+			satInput=input.getText();
+			
+		}
 		
 		
-		
+	}
+	
+	public class Clause{
+		int[] vars=new int[3];
+		boolean[] varNegs=new boolean[3];
+	
 	}
 
 }
