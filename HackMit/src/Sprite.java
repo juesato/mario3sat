324821f820d2454501f2ac6a8921e41b37a182fb
shomberg <shomberg@Nazgul>
2014-10-05 01:20:22 -0400

    Lots more Sprite infrastructure and some GameFrame infrastructure with drawing sprites in the smaller window appropriately
    Added crouching mario image.

--- HackMit/src/Sprite.java
+++ HackMit/src/Sprite.java
@@ -1,5 +1,9 @@
-import java.awt.image.BufferedImage;
+import java.awt.*;
+import java.awt.image.*;
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Sprite {
 	public double x,y;
@@ -9,31 +13,49 @@ public class Sprite {
 	private BufferedImage im;
 	public int id;
 	
-	public final int MARIO=0;
-	public final int KOOPA=1;
-	public final int KOOPA_SHELL=2;
-	public final int GUMBA=3;
-	public final int MUSHROOM=4;
-	public final int QUESTION_BLOCK=5;
-	public final int BRICK=6;
-	public final int BLOCK=7;
-	public final int SUPER_MARIO=8;
+	public static final int MARIO=0;
+	public static final int KOOPA=1;
+	public static final int KOOPA_SHELL=2;
+	public static final int GOOMBA=3;
+	public static final int MUSHROOM=4;
+	public static final int QUESTION_BLOCK=5;
+	public static final int BRICK=6;
+	public static final int BLOCK=7;
+	public static final int SUPER_MARIO=8;
+	public static final int CROUCH_MARIO=9;
 	
-	public Sprite(){
+	public Sprite(int idin){
+		this.id=idin;
+		File imf;
+		switch(id){
+		case KOOPA: imf=new File("koopa_red.gif"); break;
+		case KOOPA_SHELL: imf=new File("koopa_shell_red.gif"); break;
+		case GOOMBA: imf=new File("goomba.jpg"); break;
+		case MUSHROOM: imf=new File("mushroom.png"); break;
+		case QUESTION_BLOCK: imf=new File("mystery_box.gif"); break;
+		case BRICK: imf=new File("brick.png"); break;
+		case BLOCK: imf=new File("empty_block.gif"); break;
+		case SUPER_MARIO: imf=new File("mario_large.gif"); break;
+		case CROUCH_MARIO: imf=new File("mario_crouch.gif"); break;
+		default: imf=new File("mario_small.gif"); break;
+		}
+		try {
+			im=ImageIO.read(imf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public Sprite(int stageXIn,int stageYIn){
-		this();
+	public Sprite(int stageXIn,int stageYIn, int idin){
+		this(idin);
 		this.stageX=stageXIn;
 		this.stageY=stageYIn;
 	}
 	
-	public Sprite(int idin){
-		this.id=idin;
-	}
-	
-	public Sprite(int stageXIn,int stageYIn, int idin){
-		this.id=idin;
+	public Sprite(double xIn, double yIn, int stageXIn, int stageYIn, int idin){
+		this(stageXIn, stageYIn, idin);
+		x = xIn;
+		y = yIn;
 	}
 	
 	public boolean checkHorCollide(Sprite in){
@@ -63,5 +85,14 @@ public class Sprite {
 	}
 	public void setY(int pixelY){
 		y = ((double)pixelY)/stageY;
+	}
+	
+	public void draw(Graphics g, int winX, int winY, int winWidth, int winHeight){
+		int pixelX = getPixelX();
+		int pixelY = getPixelY();
+		System.out.println("X is: " + pixelX + " and Y is: " + pixelY);
+		if(pixelX+width>=winX && pixelY+height>=winY && pixelX<=winX+winWidth && pixelY<=winY+winHeight){
+			g.drawImage(im, pixelX-winX, pixelY-winY, null);
+		}
 	}
 }
