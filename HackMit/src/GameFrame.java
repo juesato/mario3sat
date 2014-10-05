c1f64b32a56a9a5fc756b7be25aefec29daa6d89
Eugene Chen <eugene.chen@addepar.com>
2014-10-05 04:55:34 -0400

    asdf

--- HackMit/src/GameFrame.java
+++ HackMit/src/GameFrame.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GameFrame extends JFrame {
-<<<<<<< HEAD
 
   GamePane gp;
   ArrayList<Sprite> sprites;
@@ -33,19 +32,15 @@ public class GameFrame extends JFrame {
     winX = 0;
     winY = 0;
 
-    // sprites = new ArrayList<Sprite>();
+    sprites = new ArrayList<Sprite>();
     movingSprites = new ArrayList<Sprite>();
-    // for (int i = 0; i < 20; i++) {
-    // for (int j = 0; j < 20; j++) {
-    // if (i == 0 || i == 19 || j == 0 || j == 19)
-    // sprites
-    // .add(new Sprite(i * 32.0 / stageX, j * 32.0 / stageY, stageX, stageY, Sprite.BLOCK));
-    // }
-    // }
-
-    Gadget g = new Gadget("CROSSOVER", true);
-    sprites = g.getSprites();
-
+    for (int i = 0; i < 20; i++) {
+      for (int j = 0; j < 20; j++) {
+        if (i == 0 || i == 19 || j == 0 || j == 19)
+          sprites
+              .add(new Sprite(i * 32.0 / stageX, j * 32.0 / stageY, stageX, stageY, Sprite.BLOCK));
+      }
+    }
 
     mario = new Sprite(100.0 / stageX, 100.0 / stageY, stageX, stageY, Sprite.MARIO);
     sprites.add(mario);
@@ -81,13 +76,19 @@ public class GameFrame extends JFrame {
     public void keyTyped(KeyEvent e) {}
 
     public void keyPressed(KeyEvent e) {
-      System.out.println("key pressed");
+      // System.out.println("key pressed");
       if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-        mario.hspeed = -128.0 / stageX;
+        mario.hspeed = -256.0 / stageX;
       } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-        mario.hspeed = 128.0 / stageX;
+        mario.hspeed = 256.0 / stageX;
       } else if (e.getKeyCode() == KeyEvent.VK_UP) {
-        mario.vspeed = -500.0 / stageY;
+        if (!mario.airborne) {
+          mario.airborne = true;
+          mario.vspeed = -500.0 / stageY;
+        }
+      } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+        if (mario.id == Sprite.SUPER_MARIO)
+          mario.id = Sprite.CROUCH_MARIO;
       }
     }
 
@@ -98,6 +99,9 @@ public class GameFrame extends JFrame {
       } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
         if (mario.hspeed > 0)
           mario.hspeed = 0;
+      } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+        if (mario.id == Sprite.CROUCH_MARIO)
+          mario.id = Sprite.SUPER_MARIO;
       }
     }
   }
@@ -106,11 +110,7 @@ public class GameFrame extends JFrame {
     boolean done = false;
     int freq = 60;
     long stepSize = 1000 / freq;
-
-    Gadget g = new Gadget("CROSSOVER", true);
-    ArrayList<Sprite> sprites = g.getSprites();
-
-    // ArrayList<Sprite> sprites;
+    ArrayList<Sprite> sprites;
     ArrayList<Sprite> movingSprites;
 
     public GameThread(ArrayList<Sprite> spritesIn, ArrayList<Sprite> movingSpritesIn) {
@@ -137,7 +137,6 @@ public class GameFrame extends JFrame {
                 if (sprite.hspeed > 0)
                   sprite.setX(sprite2.getPixelX() - sprite.width);
                 else
-
                   sprite.setX(sprite2.getPixelX() + sprite2.width);
                 sprite.hspeed *= -1;
               }
@@ -199,8 +198,10 @@ public class GameFrame extends JFrame {
                 sprite.vspeed = 0;
               }
               if (sprite.id == Sprite.MARIO) {
-                if (sprite.vspeed > 0)
+                if (sprite.vspeed > 0) {
                   sprite.setY(sprite2.getPixelY() - sprite.height);
+                  sprite.airborne = false;
+                }
                 else
                   sprite.setY(sprite2.getPixelY() + sprite2.height);
                 if (sprite.vspeed > 0)
@@ -309,302 +310,4 @@ public class GameFrame extends JFrame {
       }
     }
   }
-=======
-	
-	GamePane gp;
-	ArrayList<Sprite> sprites;
-	ArrayList<Sprite> movingSprites;
-	Sprite mario;
-	int winWidth, winHeight;
-	int winX, winY;
-	int stageX,stageY;
-	
-	public GameFrame(){
-		super("3-Sat as Mario");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		gp = new GamePane();
-		setContentPane(gp);
-		winWidth = 32*20;
-		winHeight = 32*20;
-		setSize(winWidth, winHeight);
-		
-		stageX = 32*40;
-		stageY = 32*40;
-		
-		winX = 0;
-		winY = 0;
-		
-		sprites = new ArrayList<Sprite>();
-		movingSprites = new ArrayList<Sprite>();
-		for(int i = 0; i < 40; i++){
-			for(int j = 0; j < 40; j++){
-				if(i==0||i==39||j==0||j==39)
-					sprites.add(new Sprite(i*32.0/stageX, j*32.0/stageY, stageX, stageY, Sprite.BLOCK));
-			}
-		}
-		
-		mario = new Sprite(100.0/stageX, 100.0/stageY, stageX, stageY, Sprite.MARIO);
-		sprites.add(mario);
-		movingSprites.add(mario);
-		
-		GameThread gt = new GameThread(sprites, movingSprites);
-		gt.start();
-		
-		setVisible(true);
-		gp.requestFocusInWindow();
-	}
-	
-	public class GamePane extends JPanel implements KeyListener{
-		public GamePane(){
-			addKeyListener(this);
-		}
-		
-		public void paintComponent(Graphics g){
-//			try {
-//				BufferedImage img = ImageIO.read(new File("brick.png"));
-//				g.drawImage(img, 64,64,Color.white, null);
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-			g.fillRect(0, 0, winWidth, winHeight);
-			for(Sprite s : sprites){
-//				System.out.println("Drawing " + s);
-				s.draw(g, winX, winY, winWidth, winHeight);
-			}
-		}
-
-		public void keyTyped(KeyEvent e) {
-		}
-
-		public void keyPressed(KeyEvent e) {
-//			System.out.println("key pressed");
-			if(e.getKeyCode()==KeyEvent.VK_LEFT){
-				mario.hspeed=-175.0/stageX;
-			}else if(e.getKeyCode()==KeyEvent.VK_RIGHT){
-				mario.hspeed=175.0/stageX;
-			}else if(e.getKeyCode()==KeyEvent.VK_UP){
-				if(!mario.airborne){
-					mario.airborne = true;
-					mario.vspeed=-500.0/stageY;
-				}
-			}else if(e.getKeyCode()==KeyEvent.VK_DOWN){
-				if(mario.id==Sprite.SUPER_MARIO)
-					mario.id=Sprite.CROUCH_MARIO;
-			}
-		}
-
-		public void keyReleased(KeyEvent e) {
-			if(e.getKeyCode()==KeyEvent.VK_LEFT){
-				if(mario.hspeed<0)
-					mario.hspeed=0;
-			}else if(e.getKeyCode()==KeyEvent.VK_RIGHT){
-				if(mario.hspeed>0)
-					mario.hspeed=0;
-			}else if(e.getKeyCode()==KeyEvent.VK_DOWN){
-				if(mario.id==Sprite.CROUCH_MARIO)
-					mario.id=Sprite.SUPER_MARIO;
-			}
-		}
-	}
-	
-	public class GameThread extends Thread{
-		boolean done = false;
-		int freq=60;
-		long stepSize = (long)(1000/freq);
-		ArrayList<Sprite> sprites;
-		ArrayList<Sprite> movingSprites;
-		
-		public GameThread(ArrayList<Sprite> spritesIn,ArrayList<Sprite> movingSpritesIn){
-			this.sprites=spritesIn;
-			this.movingSprites=movingSpritesIn;
-		}
-		
-		public void run(){
-			
-			while(!done){
-				long timestamp = System.currentTimeMillis();
-				//Begin step computations
-				for(Sprite sprite : movingSprites){
-					sprite.x+=sprite.hspeed/freq;
-					for(Sprite sprite2:sprites){
-						if(sprite2==sprite) continue;
-						if(sprite.checkHorCollide(sprite2) && sprite.checkVertCollide(sprite2)){
-							System.out.println("hor collision between " + sprite + " and " + sprite2);
-							if(sprite.id==Sprite.KOOPA_SHELL){
-								if(sprite2.id==Sprite.BRICK){
-									sprites.remove(sprite2);
-								}
-								if(sprite.hspeed>0)
-									sprite.setX(sprite2.getPixelX()-sprite.width);
-								else
-									sprite.setX(sprite2.getPixelX()+sprite2.width);
-								sprite.hspeed*=-1;
-							}
-							if(sprite.id==Sprite.MARIO){
-								if(sprite.hspeed>0)
-									sprite.setX(sprite2.getPixelX()-sprite.width);
-								else
-									sprite.setX(sprite2.getPixelX()+sprite2.width);
-								
-								
-								if(sprite2.id==Sprite.KOOPA){
-									
-									sprites.add(new Sprite(sprite2.x,sprite2.y,stageX,stageY,Sprite.KOOPA_SHELL,Sprite.SHELL_SPEED));
-									sprites.remove(sprite2);
-								}
-								
-								if(sprite2.id==Sprite.GOOMBA){
-									sprites.remove(sprite2);
-									
-								}
-								if(sprite2.id==Sprite.MUSHROOM){
-									sprites.add(new Sprite(sprite.x,sprite.y,stageX,stageY,Sprite.SUPER_MARIO));
-									sprites.remove(sprite);
-								}
-							}
-							if(sprite.id==Sprite.SUPER_MARIO){
-								if(sprite.hspeed>0)
-									sprite.setX(sprite2.getPixelX()-sprite.width);
-								else
-									sprite.setX(sprite2.getPixelX()+sprite2.width);
-								if(sprite2.id==sprite.KOOPA){
-									sprites.add(new Sprite(sprite2.x,sprite2.y,stageX,stageY,Sprite.KOOPA_SHELL,Sprite.SHELL_SPEED));
-									sprites.remove(sprite2);
-								}
-								if(sprite2.id==Sprite.GOOMBA){
-									sprites.add(new Sprite(sprite.x,sprite.y,stageX,stageY,Sprite.MARIO));
-									sprites.remove(sprite);
-									sprites.remove(sprite2);
-									
-								}
-							}
-						}
-					}
-
-					sprite.vspeed+=Sprite.gravity/stageY/freq;
-					sprite.y+=sprite.vspeed/freq;
-
-					for(Sprite sprite2:sprites){
-						if(sprite2==sprite) continue;
-						if(sprite.checkVertCollide(sprite2) && sprite.checkHorCollide(sprite2)){
-//							System.out.println("vert collision between " + sprite + " and " + sprite2);
-							if(sprite.id==Sprite.KOOPA_SHELL){								
-								if(sprite.vspeed>0)
-									sprite.setY(sprite2.getPixelY()-sprite.height);
-								else
-									sprite.setY(sprite2.getPixelY()+sprite2.height);
-								sprite.vspeed=0;
-							}
-							if(sprite.id==Sprite.MARIO){
-								if(sprite.vspeed>0){
-									sprite.setY(sprite2.getPixelY()-sprite.height);
-									sprite.airborne=false;
-								}
-								else
-									sprite.setY(sprite2.getPixelY()+sprite2.height);
-								if(sprite.vspeed>0)
-									sprite.vspeed=0;
-								
-								if(sprite2.id==Sprite.KOOPA){
-									sprites.add(new Sprite(sprite2.x,sprite2.y,stageX,stageY,Sprite.KOOPA_SHELL,Sprite.SHELL_SPEED));
-									sprites.remove(sprite2);
-									sprite.vspeed*=-1;
-								}
-								if(sprite2.id==Sprite.GOOMBA){
-									sprites.remove(sprite2);
-								}
-								if(sprite2.id==Sprite.MUSHROOM){
-									sprites.add(new Sprite(sprite.x,sprite.y,stageX,stageY,Sprite.SUPER_MARIO));
-									sprites.remove(sprite);
-								}
-								if(sprite2.id==Sprite.QUESTION_BLOCK){
-									if(sprite.vspeed<0){
-										sprites.add(new Sprite(sprite2.x,sprite2.y,stageX,stageY,Sprite.BLOCK));
-										sprites.add(new Sprite(sprite2.x,sprite2.y-sprite2.width,stageX,stageY,Sprite.MUSHROOM));
-										sprites.remove(sprite2);
-									}
-								}
-								
-								
-							}
-							if(sprite.id==sprite.SUPER_MARIO){
-								if(sprite.vspeed>0)
-									sprite.setY(sprite2.getPixelY()-sprite.height);
-								else
-									sprite.setY(sprite2.getPixelY()+sprite2.height);
-								if(sprite.vspeed>0)
-									sprite.vspeed=0;
-								if(sprite2.id==sprite.KOOPA){
-									sprites.add(new Sprite(sprite2.x,sprite2.y,stageX,stageY,sprite.KOOPA_SHELL,sprite.SHELL_SPEED));
-									sprites.remove(sprite2);
-									sprite.vspeed*=-1;
-								}
-								if(sprite2.id==sprite.GOOMBA){
-									sprites.add(new Sprite(sprite.x,sprite.y,stageX,stageY,sprite.MARIO));
-									sprites.remove(sprite);
-									sprites.remove(sprite2);
-								}
-								if(sprite2.id==sprite.QUESTION_BLOCK){
-									if(sprite.vspeed<0){
-										sprites.add(new Sprite(sprite2.x,sprite2.y,stageX,stageY,sprite.BLOCK));
-										sprites.add(new Sprite(sprite2.x,sprite2.y-sprite2.width,stageX,stageY,sprite.MUSHROOM));
-										sprites.remove(sprite2);
-									}
-								}
-								if(sprite2.id==sprite.BRICK){
-									if(sprite.vspeed<0){
-										sprites.remove(sprite2);
-										sprite.vspeed=0;
-									}
-								}
-								
-							}
-							if(sprite.id==sprite.CROUCH_MARIO){
-								if(sprite.vspeed>0)
-									sprite.setY(sprite2.getPixelY()-sprite.height);
-								else
-									sprite.setY(sprite2.getPixelY()+sprite2.height);
-								
-								if(sprite2.id==sprite.KOOPA){
-									sprites.add(new Sprite(sprite2.x,sprite2.y,stageX,stageY,sprite.KOOPA_SHELL,sprite.SHELL_SPEED));
-									sprites.remove(sprite2);
-								}
-								if(sprite2.id==sprite.GOOMBA){
-									sprites.add(new Sprite(sprite.x,sprite.y,stageX,stageY,sprite.MARIO));
-									sprites.remove(sprite);
-									sprites.remove(sprite2);
-								}
-								if(sprite2.id==sprite.BRICK){
-									if(sprite.vspeed>0){
-										sprites.remove(sprite2);
-										sprite.vspeed=0;
-									}
-								}
-								if(sprite.vspeed>0)
-									sprite.vspeed=0;
-							}
-						}
-						
-					}
-				}
-				//End step computations
-				winX = mario.getPixelX()-winWidth/2;
-				winY = mario.getPixelY()-winWidth/2;
-				gp.repaint();
-				long left = timestamp + stepSize - System.currentTimeMillis();
-				if(left > 0)
-					delay(left);
-			}
-		}
-		
-		public void delay(long ms){
-			try {
-				Thread.sleep(ms);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
->>>>>>> 2939074b08b031dd1857214a755bd50d379f1834
 }
